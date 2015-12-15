1d95f8568efe140f6ed9710ff8f671f133c448a4
Dewey Sasser <dewey@sasser.com>
2015-12-15 17:46:37 -0500

    Test the combat method

--- Game/Combat.py
+++ Game/Combat.py
@@ -2,10 +2,13 @@
 import random
 
 class Combat(object):
-    def __init__(self, actor1, actor2):
+    def __init__(self, actor1, actor2, seed=None):
         self.actors=[actor1, actor2]
         self.history = []
-        self.random = random.Random(0)
+        if seed is None:
+            self.random = random.Random()
+        else:
+            self.random = random.Random(seed)
 
     def fight(self):
         ''' Have the 2 actors fight, returning a history set '''
@@ -16,10 +19,12 @@ class Combat(object):
         return reduce(lambda x, y: x and y, [x.is_alive() for x in self.actors])
 
     def exchange(self):
+        damage = 0
         if self.isHit(*self.actors):
             damage = self.damage(*self.actors)
             self.actors[1].hurt(damage)
-            self.actors = list(reversed(self.actors))
+        self.actors = list(reversed(self.actors))
+        return ( damage, self.actors[1], self.actors[0])
         
     def isHit(self, a1, a2):
         return a1.attack_weight() + self.random.randrange(-5, 5) > a2.defense_weight()
--- Game/testCombat.py
+++ Game/testCombat.py
@@ -4,12 +4,25 @@ from Combat import *
 
 
 class testCombat(unittest.TestCase):
+
+    def test_random(self):
+        combat = Combat(None, None, seed=0)
+
+        self.assertEquals(combat.random.randrange(0, 100), 84)
+        self.assertEquals(combat.random.randrange(0, 100), 75)
+        self.assertEquals(combat.random.randrange(0, 100), 42)
+
     def test_basic(self):
-        c1 = Character("John", hp=100)
-        c2 = Character("James", hp=50)
+        c1 = Character("John", hp=15)
+        c2 = Character("James", hp=10)
+        d = [7, 0, 0, 0, 0, 3, 0, 0, 5]
 
-        combat = Combat(c1, c2)
+        combat = Combat(c1, c2, seed=0)
 
-        combat.exchange()
+        combat.fight()
 
-        self.assertTrue(c2.hp() < 50)
+        self.assertEquals(len(combat.history), 9)
+        results = [x[0] for x in combat.history]
+
+        self.assertEquals(results, d)
+
